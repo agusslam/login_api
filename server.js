3e3bafed0190e532cd7145c135fa921cb93cae9f
agusslam <agus.slamethermawan@gmail.com>
2021-11-30 21:39:56 +0700

    update set view

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(cors())
 
 app.use(routes)
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './Views'));
 app.use('/', express.static('public'));
 
 app.listen(app.get('port'), function() {
